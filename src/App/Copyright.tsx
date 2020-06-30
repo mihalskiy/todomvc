4154d78cec7361267655dbc0e22befeb99ac4e51
mihalskiy <mihalskiy.rv@gmail.com>
2020-06-30 13:47:09 +0300

    add pulumi - Modern Infrastructure as Code. autoload

--- src/App/Copyright.tsx
+++ src/App/Copyright.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import * as packages from '../../package.json'
 
 const Copyright: React.FC = () => (
   <footer className="info">
     <p>
-      Created by <a href="https://ryota-murakami.github.io/">Ryota Murakamai</a>
+      Created by <a href="https://ryota-murakami.github.io/">{packages.name}</a>
     </p>
     <p>
-      Part of <a href="http://todomvc.com">TodoMVC</a>
+      Part of <a href="http://todomvc.com">{packages.name}</a>
     </p>
   </footer>
 )
